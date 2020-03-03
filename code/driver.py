93ced451d26481e2fa582fb9839212f6f00b613d
Yucheng Zhang <ustczyc@outlook.com>
2020-03-03 17:02:58 -0500

    add continuing mcmc run from chain

--- code/driver.py
+++ code/driver.py
@@ -3,16 +3,14 @@ from . import prob
 from . import utils
 
 
-def run_mcmc(pm, nwalkers, burnin, nsteps, froot, progress=False):
-    '''main function for runnning MCMC'''
-    pm.ini_dm()
+def run_mcmc(pm, nwalkers, nsteps, fn_h5, progress=False):
+    '''Main function for runnning MCMC.'''
     ndim = pm.npars
     # starting point for each walker
     print('>> Initializing walkers...')
     p0 = pm.ini_pars(nwalkers)
 
     # set up the backend for hdf5 chain file
-    fn_h5 = froot + '.h5'
     backend = emcee.backends.HDFBackend(fn_h5)
     backend.reset(nwalkers, ndim)
 
@@ -22,28 +20,27 @@ def run_mcmc(pm, nwalkers, burnin, nsteps, froot, progress=False):
 
     # run MCMC
     print('>> Running MCMC, HDF5 chain file: {0:s}'.format(fn_h5))
-    _state = sampler.run_mcmc(p0, burnin+nsteps,
-                              store=True, progress=progress)
-
-    # write chain to txt data file
-    fo_chain = froot + '_chain.dat'
-    utils.hdf5_to_txt(fn_h5, fo_chain, pm.pars['pars'], burnin=burnin)
-
-    # get statistics
-    fo_stat = froot + '_stat.dat'
-    utils.get_stat(fo_chain, fo_stat, pm.pars['pars'], dof=pm.dof)
+    _state = sampler.run_mcmc(p0, nsteps, progress=progress)
 
 
-def run_stat(pm, froot, burnin):
-    '''get chain and stat from hdf5'''
-    pm.ini_dm()
+def cont_run(pm, fn_chain, nsteps, progress=False):
+    '''Continue the mcmc run w/ a chain.
+    nwalkers is determined from the chain.'''
+    # load the chain
+    backend = emcee.backends.HDFBackend(fn_chain)
+    nwalkers, ndim = backend.shape[0], backend.shape[1]
+    print('>> Info of the chain <<')
+    print('>> chain file: {:s}'.format(fn_chain))
+    print('>> Number of walkers: {:d}'.format(nwalkers))
+    print('>> Number of parameters: {:d}'.format(ndim))
+    print('>> Iterations: {:d}'.format(backend.iteration))
 
-    fn_h5 = froot + '.h5'
+    # simple check on the num. of parameters
+    if pm.npars != ndim:
+        raise ValueError('# pars in config: {:d}'.format(pm.npars))
 
-    # write chain to txt data file
-    fo_chain = froot + '_chain.dat'
-    utils.hdf5_to_txt(fn_h5, fo_chain, pm.pars['pars'], burnin=burnin)
-
-    # get statistics
-    fo_stat = froot + '_stat.dat'
-    utils.get_stat(fo_chain, fo_stat, pm.pars['pars'], dof=pm.dof)
+    # continue the run
+    print('>> Running MCMC ...')
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, pm.ln_prob,
+                                    backend=backend)
+    _state = sampler.run_mcmc(None, nsteps, progress=True)
--- fitrsd.py
+++ fitrsd.py
@@ -3,6 +3,7 @@
 import argparse
 from code import prob
 from code import driver
+from code import utils
 import sys
 
 if __name__ == "__main__":
@@ -10,14 +11,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Fit RSD w/ CLPT GSRSD model.')
     parser.add_argument('-ini_file', type=str, default=None)
-    parser.add_argument('-nwalkers', type=int, default=10)
-    parser.add_argument('-froot', type=str, default='fitrsd')
-    parser.add_argument('-burnin', type=int, default=10,
-                        help='burnin steps, which will not be saved to chain file')
-    parser.add_argument('-nsteps', type=int, default=10,
-                        help='number of steps for each walker')
+    parser.add_argument('-run', type=int, default=0,
+                        help='1: run mcmc \n' +
+                        '2: continue the run')
+    parser.add_argument('-froot', type=str, default='fitrsd',
+                        help='root for output files')
+    parser.add_argument('-nwalkers', type=int, default=100)
+    parser.add_argument('-nsteps', type=int, default=1000,
+                        help='number of steps for each walker, including burnin')
     parser.add_argument('-progress', type=int, default=0)
-    parser.add_argument('-run_mcmc', type=int, default=1)
+
+    # get chain related
+    parser.add_argument('-get_chain', type=int, default=0,
+                        help='get chain from .h5 file')
+    parser.add_argument('-burnin', type=int, default=500,
+                        help='burnin steps removed')
     args = parser.parse_args()
 
 
@@ -29,9 +37,20 @@ if __name__ == "__main__":
         sys.exit('-ini_file must be specified')
 
     pm = prob.prob(args.ini_file)  # the probability model
+    pm.ini_dm()
 
-    if bool(args.run_mcmc):
-        driver.run_mcmc(pm, args.nwalkers, args.burnin, args.nsteps,
-                        args.froot, progress=bool(args.progress))
+    fn_h5 = args.froot + '.h5'
+
+    if args.run == 1:
+        driver.run_mcmc(pm, args.nwalkers, args.nsteps,
+                        fn_h5, progress=bool(args.progress))
+    elif args.run == 2:
+        driver.cont_run(pm, fn_h5, args.nsteps, progress=bool(args.progress))
     else:
-        driver.run_stat(pm, args.froot, args.burnin)
+        pass
+
+    if bool(args.get_chain):
+        fo_chain = args.froot + '_chain.dat'
+        utils.hdf5_to_txt(fn_h5, fo_chain, pm.pars['pars'], burnin=args.burnin)
+    else:
+        pass
